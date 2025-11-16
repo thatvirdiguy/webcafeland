c51977fefc92a0216553ffde434173c586b945ce
thatvirdiguy <amanCJ24@gmail.com>
2025-11-16 16:27:45 +0530

    initial homepage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import HomePage from './Views/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+export default App;
